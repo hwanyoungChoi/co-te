6576afaa003d8141103c1fc13c75a7bc3f7a551c
Cony <cony.choi@kidsnote.com>
2023-01-18 01:26:19 +0900

    [level 1] Title: 약수의 합, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    var answer = 0;
+    
+    for (let i = 1; i <= n; i++) {
+        if (n % i) {
+            continue;
+        }
+        
+        answer += i;
+    }
+    
+    return answer;
+}
