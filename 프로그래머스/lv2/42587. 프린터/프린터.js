735ee278a8d798dc174b21799d854c4537a1ebcc
Cony <cony.choi@kidsnote.com>
2023-01-18 01:01:08 +0900

    [level 2] Title: 프린터, Time: 5.15 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42587. 프린터/프린터.js
@@ -0,0 +1,30 @@
+function solution(priorities, location) {
+    let answer = 0;
+    
+    const indexList = Array.from(priorities, (_, i) => i);
+    
+    while (priorities.length > 0) {
+        // 정렬될 떄 까지 여기 로직만 실행
+        if (priorities.some((priority) => priority > priorities[0])) {
+            priorities.push(priorities.shift());
+            indexList.push(indexList.shift());
+            continue;
+        }
+        
+        answer += 1;
+        
+        console.log(indexList);
+        console.log(location);
+        if (indexList[0] === location) {
+            break;
+        }
+    
+        // 정렬 이후 여기 로직만 실행
+        if (indexList[0] !== location) {
+            priorities.shift();
+            indexList.shift();
+        }
+    }
+    
+    return answer;
+}
