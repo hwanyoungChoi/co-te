5ef0a3f7736348aefbe1191d19f3a8f94fd5cc8d
Cony <cony.choi@kidsnote.com>
2023-01-24 02:01:45 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -0,0 +1,11 @@
+function solution(numbers) {
+    let sum = 0;
+    
+    for (let i = 0; i <= 9; i++) {
+        if (!numbers.some((v) => v === i)) {
+            sum += i;
+        }
+    }
+    
+    return sum;
+}
