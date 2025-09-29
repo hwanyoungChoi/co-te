9b421dabff0c98229b03d8035cd8c15d2c89103c
Cony <welcomechoi@kakao.com>
2025-09-29 17:15:40 +0900

    [Bronze II] Title: 수 정렬하기, Time: 152 ms, Memory: 11280 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./test.txt";
+const input = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n")
+  .map(Number);
+
+const [N, ...numbers] = input;
+
+numbers.sort((a, b) => a - b).forEach((n) => console.log(n));
